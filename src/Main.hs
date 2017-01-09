630d0bababe51a0810b06445256046adaa4467dd
GiantPinkWalrus <sampolammas@gmail.com>
2017-01-09 07:42:22 +0200

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,67 @@
+module Main where
+
+import Control.Arrow
+import Control.Exception
+import Control.Monad.Reader
+import Data.List
+import Network
+import System.IO
+import System.Exit
+import Text.Printf
+
+
+server = "localhost"
+port = 6667
+chan = "#test"
+nick = "Baronbot"
+
+data Bot = Bot { socket :: Handle }
+type Net = ReaderT Bot IO
+
+main :: IO ()
+main = bracket connect disconnect loop
+  where
+    disconnect = hClose . socket
+    loop st    = runReaderT run st
+
+connect :: IO Bot
+connect = notify $ do
+    h <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering h NoBuffering
+    return (Bot h)
+  where
+    notify a = bracket_
+        (printf "Connecting to %s ..." server >> hFlush stdout)
+        (putStrLn "done.")
+        a
+
+run :: Net ()
+run = do
+    write "NICK" nick
+    write "USER" (nick++" 0 * :tutorial bot")
+    write "JOIN" chan
+    asks socket >>= listen
+
+write ::String -> String -> Net ()
+write s t = do
+    h <- asks socket
+    io $ hPrintf h "%s %s\r\n" s t
+    io $ printf    "> %s %s\n" s t
+
+io :: IO a -> Net a
+io = liftIO
+
+listen :: Handle -> Net ()
+listen h = forever $ do
+    s <- init `fmap` io (hGetLine h)
+    io (putStrLn s)
+    if ping s then pong s else eval (clean s)
+  where
+    forever a = a >> forever a
+    clean     = drop 1 . dropWhile (/= ':') . drop 1
+    ping x    = "PING :" `isPrefixOf` x
+    pong x    = write "PONG" (':' : drop 6 x)
+
+eval :: String -> Net ()
+eval "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval _       = return ()
