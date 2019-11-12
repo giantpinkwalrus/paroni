09259fe9911fb88daa793bd37821bd7a14d2bfe1
Sampo Lamminsalo <sampo.lamminsalo@gofore.com>
2019-11-12 16:40:42 +0200

    Project builds and connects to an IRC server.
    Update libraries, rewrite things.

--- src/Main.hs
+++ src/Main.hs
@@ -1,74 +1,60 @@
 module Main where
 
 import Control.Arrow
+import Control.Applicative
 import Control.Concurrent
-import Control.Exception
+import Control.Monad
 import Control.Monad.Reader
-import Data.List
-import Network
+import qualified Control.Exception as E
+import qualified Data.ByteString.UTF8 as B8
+import Data.Void
+import Network.Socket hiding (recv)
+import Network.Socket.ByteString (recv, sendAll)
 import System.IO
 import System.Exit
-import Text.Printf
 
+import Text.Megaparsec hiding (State)
+import Text.Megaparsec.Byte
+import qualified Text.Megaparsec.Byte.Lexer as L
+
+type Parser = Parsec Void B8.ByteString
 
 server = "localhost"
-port = 6667
+port = "32771"
 chan = "#test"
 nick = "Baronbot"
 
-data Bot = Bot { socket :: Handle }
-type Net = ReaderT Bot IO
-
 main :: IO ()
-main = do
-    spawnUi
-    bracket connect disconnect loop
+main = withSocketsDo $ do
+    addr <- resolve server port
+    E.bracket (conn addr) disconnect talk
   where
-    disconnect = hClose . socket
-    loop st    = runReaderT run st
+    disconnect   = close
+    resolve host port = do
+      let hints = defaultHints { addrSocketType = Stream }
+      addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+      return addr
+    conn addr = do
+      sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+      connect sock $ addrAddress addr
+      return sock
+    talk sock = do
+      rmsg <- recv sock 1024
+      let msg = B8.toString rmsg
+      printMsg msg
+      handleMsg msg
+      talk sock
 
-spawnUi = forkIO $ forever $ do
-    val <- getLine
-    write "" val
+printMsg :: String -> IO ()
+printMsg "" = putStr ""
+printMsg msg = putStrLn msg
 
-connect :: IO Bot
-connect = notify $ do
-    h <- connectTo server (PortNumber (fromIntegral port))
-    hSetBuffering h NoBuffering
-    return (Bot h)
-  where
-    notify a = bracket_
-        (printf "Connecting to %s ..." server >> hFlush stdout)
-        (putStrLn "done.")
-        a
+getPrefix :: String -> String
+getPrefix = takeWhile(/= ' ')
 
-run :: Net ()
-run = do
-    write "NICK" nick
-    write "USER" (nick++" 0 * :tutorial bot")
-    write "JOIN" chan
-    asks socket >>= listen
+getMessage = dropWhile(/= ' ')
 
-write :: String -> String -> Net ()
-write s t = do
-    h <- asks socket
-    io $ hPrintf h "%s %s\r\n" s t
-    io $ printf    "> %s %s\n" s t
+handleMsg msg
+  | getPrefix msg == "ERROR" = error $ getMessage msg
+  | otherwise = return ()
 
-io :: IO a -> Net a
-io = liftIO
-
-listen :: Handle -> Net ()
-listen h = forever $ do
-    s <- init `fmap` io (hGetLine h)
-    io (putStrLn s)
-    if ping s then pong s else eval (clean s)
-  where
-    forever a = a >> forever a
-    clean     = drop 1 . dropWhile (/= ':') . drop 1
-    ping x    = "PING :" `isPrefixOf` x
-    pong x    = write "PONG" (':' : drop 6 x)
-
-eval :: String -> Net ()
-eval "!quit" = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
-eval _       = return ()
